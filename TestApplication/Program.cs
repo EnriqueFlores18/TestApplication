d699131734fadeaebd30deb20909591cd94c5af3
Salem Jatić <salem.jatic205181@student.leeuwenborgh.nl>
2017-10-05 13:20:00 +0200

    lol

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -11,6 +11,7 @@ namespace TestApplication
         static void Main(string[] args)
         {
             Console.WriteLine(" Ayeeee Biiiitchhhh");
+            Console.ReadLine();
         }
     }
 }
